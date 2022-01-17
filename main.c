096f433332bf756cbdaee32b69456acf4816a911
loubnaitsmail <loubna.itsmail.1993@gmail.com>
2022-01-17 20:55:04 +0100

    une pipe

---
+++ main.c
@@ -0,0 +1,114 @@
+#include "pipex.h"
+
+void	error(void)
+{
+	perror("\033[31mError");
+	exit(EXIT_FAILURE);
+}
+
+char	*find_path(char *cmd, char **envp)
+{
+	char	**paths;
+	char	*path;
+	int		i;
+	char	*part_path;
+
+	i = 0;
+	while (ft_strnstr(envp[i], "PATH", 4) == 0)
+		i++;
+	paths = ft_split(envp[i] + 5, ':');
+	i = 0;
+	while (paths[i])
+	{
+		part_path = ft_strjoin(paths[i], "/");
+		path = ft_strjoin(part_path, cmd);
+		free(part_path);
+		if (access(path, F_OK) == 0)
+			return (path);
+		i++;
+	}	
+	return (0);
+}
+
+void	execute_cmd(char *argv, char **envp)
+{
+	char	**cmd;
+	char	*final_path;
+
+	cmd = ft_split(argv, ' ');
+	final_path = find_path(cmd[0], envp);
+	if (execve(final_path, cmd, envp) == -1)
+		error();
+}
+
+//fd[0] = STDIN >>> infile 
+//fd[1] = STDOUT >>> end[1]
+//fd[2] = STDERR
+//fd[3] = infile 
+////fd[4] = outfile
+//end[0] 
+//end[1] > Writeoutput of cmd1
+
+void	child_process(char **argv, char **envp, int *end)
+{
+	int		infile;
+
+	infile = open(argv[1], O_RDONLY | O_CREAT, 0777);
+	if (infile == -1)
+		error();
+	if (dup2(infile, STDIN_FILENO) < 0)
+		error();
+	if (dup2(end[1], STDOUT_FILENO) < 0)
+		error();
+	close(end[0]);
+	//close(infile);
+	execute_cmd(argv[2], envp);
+}
+
+//fd[0] = STDIN >> end[0] has read from end[1] 
+//fd[1] = STDOUT >> outfile of cmd2
+//fd[2] = STDERR
+//fd[3] = infile 
+//fd[4] = outfile
+//end[0] >> Read from end[1] and write to outfile
+//end[1] = outfile of cmd1
+
+void	parent_process(char **argv, char **envp, int *end)
+{
+	int		outfile;
+
+	outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (outfile == -1)
+		error();
+	if (dup2(outfile, STDOUT_FILENO) < 0)
+		error();
+	if (dup2(end[0], STDIN_FILENO) < 0)
+		error();
+	close(end[1]);
+	//close(outfile);
+	execute_cmd(argv[3], envp);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	int	end[2];
+	int	pid1;
+
+	if (argc == 5)
+	{
+		if (pipe(end) == -1)
+			error();
+		pid1 = fork();
+		if (pid1 == -1)
+			error();
+		if (pid1 == 0)
+			child_process(argv, envp, end);
+		waitpid(pid1, NULL, 0);
+		parent_process(argv, envp, end);
+		close(end[0]);
+		close(end[1]);
+	}
+	else
+		printf("bad arguments\n");
+	return (0);
+}
